ac2b19a7a3dbb7ad4ebe10dcf266e5b81b32e838
hemlock5736 <137481609+hemlock5736@users.noreply.github.com>
2024-08-04 16:06:07 +0900

    Format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "simple-import-sort", "import"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
   },
-}
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+};
